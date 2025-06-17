accb1d38aeb6e06c7ea7a73cad249508474fedd9
JulianMbp <julian_mtb993@outlook.es>
2025-06-17 11:01:11 -0500

    feat(Dockerfile): Fix Errpr Dpckerfile with dist and tsconfig

--- user-service/src/auth/guards/jwt-auth.guard.ts
+++ user-service/src/auth/guards/jwt-auth.guard.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -14,7 +15,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
 
   canActivate(context: ExecutionContext) {
     // Verificar si la ruta está marcada como pública
-    const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
